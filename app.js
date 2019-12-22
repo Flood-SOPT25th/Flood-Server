6e2081658117ec5198db9d3e16abb0992a331e85
donghunee <ehdgns1766@naver.com>
2019-12-23 03:29:23 +0900

    몽고 디비 초기 설정

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
