dfa4d2b2f31a75b22e5ce178d6da7cd7bf714f88
Choi Young Hoon <c_yh0327@naver.com>
2019-12-24 03:18:45 +0900

    회원가입 구현( 조직생성은 미완성)

---
+++ Module/jwt.js
@@ -0,0 +1,49 @@
+const randToken = require('rand-token');
+const jwt = require('jsonwebtoken');
+const {secretOrPrivateKey} = require('../config/secretKey');
+const options = {
+    algorithm:"HS256",
+    expiresIn:"7d",
+    issuer:"Flood"
+};
+module.exports ={
+    sign:(user) =>{
+        const payload = {
+            email:user.email,
+            name:user.name,
+            group:user.group
+        };
+        const result = {
+            token:jwt.sign(payload, secretOrPrivateKey, options),
+            refreshToken:randToken.uid(256)
+        };
+        return result;
+
+    },
+    verify:(token) =>{
+        let decoded;
+    try{
+        decoded = jwt.verify(token,secretOrPrivateKey);
+    } catch(err){
+        if(err.message === 'jwt expired'){
+            console.log('expired token');
+            return -2;
+        }else if(err.message === 'invalid token') {
+            console.log("invalid token");
+            return -2;
+        } else{
+            console.log("invalid token");
+            return -2;
+        }
+    }
+    return decoded;
+    },
+    refresh:(user) =>{
+        const payload = {
+            idx:user.idx,
+            grade:user.grade,
+            name:user.name
+        };
+        return jwt.sign(payload,secretOrPrivateKey, options);
+    }
+}
---
+++ routes/auth/index.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+
+router.use('/', require('./auth'));
+
+
+module.exports = router;
