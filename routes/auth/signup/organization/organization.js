3edb00ba7208735037f6ef792e5244a077129f80
donghunee <ehdgns1766@naver.com>
2019-12-26 23:53:37 +0900

    aws

--- routes/auth/signup/organization/organization.js
+++ routes/auth/signup/organization/organization.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var randomCode = require('../../../../module/randomCode.js');
+var randomCode = require('../../../../module/randomCode');
 const groups = require('../../../../model/group');
 var upload = require('../../../../module/awsUpload');
 
