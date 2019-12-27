5d2a042e41f98478cd5b678a29501285f0d9191e
donghunee <ehdgns1766@naver.com>
2019-12-28 01:48:51 +0900

    에러 핸들링

--- routes/group/category.js
+++ routes/group/category.js
@@ -7,17 +7,23 @@ var jwt = require('../../module/jwt')
 var statusCode = require('../../module/statusCode')
 var authUtils = require('../../module/authUtils')
 
-// 그룹 코드 조회
+// 그룹 카테고리 조회
 router.get('/',authUtils.LoggedIn, async (req, res, next) => {
     const userEmail = req.userEmail 
-    let result = await user.findOne({email:userEmail}).select({groupCode: 1})
-    let category = await group.findOne({groupCode : result.groupCode}).select({category : 1}) 
-    res.status(200).json({
-        message:"그룹 코드 조회 성공",
-        data : {
-            category : category.category
-        }
-    })
+    try {
+        let result = await user.findOne({email:userEmail}).select({groupCode: 1})
+        let category = await group.findOne({groupCode : result.groupCode}).select({category : 1}) 
+        res.status(200).json({
+            message:"그룹 카테고리 조회 성공",
+            data : {
+                category : category.category
+            }
+        })
+    } catch {
+        res.status(500).json({
+            message:"서버 에러"
+        })
+    }
 })
 
 module.exports = router;
--- routes/post/post.js
+++ routes/post/post.js
@@ -14,50 +14,74 @@ var authUtils = require('../../module/authUtils')
 router.get('/top', authUtils.LoggedIn ,async (req,res,next) => {
 
     const userEmail = req.userEmail 
-    let codeResult = await user.findOne({email:userEmail}).select({groupCode: 1})
+    try {
+        let codeResult = await user.findOne({email:userEmail}).select({groupCode: 1})
 
-    let result = await post.find({groupCode:codeResult.groupCode})
+        let result = await post.find({groupCode:codeResult.groupCode})
+        
+        result.sort((a, b) => { 
+            return a.score < b.score ? 1 : a.score > b.score ? -1 : 0;  
+        });
+
+        let fin_result = result.slice(0,3)
+
+        fin_result.forEach((n) => {
+            let count = n.bookmark_list.findIndex(i => i == userEmail);
+            if(count !== -1) {
+                n.bookmarked = true
+            }else {
+                n.bookmarked = false  
+            }
+        })
+
+        res.status(200).json({
+            message: "top3 피드 조회 성공",
+            data: {
+                topArr : fin_result
+            }
+        })
+    } catch  {
+        res.status(500).json({
+            message: "서버 에러"
+        })
+    }
     
-    result.sort((a, b) => { 
-        return a.score < b.score ? 1 : a.score > b.score ? -1 : 0;  
-    });
-
-    let fin_result = result.slice(0,3)
-
-    fin_result.forEach((n) => {
-        let count = n.bookmark_list.findIndex(i => i == userEmail);
-        if(count !== -1) {
-            n.bookmarked = true
-        }else {
-            n.bookmarked = false  
-        }
-    })
-
-    res.status(200).json({
-        message: "top3 피드 조회 성공",
-        data: {
-            topArr : fin_result
-        }
-    })
 })
 
 // 해당 게시물 조회 및 조회수 증가 # 완료
 router.get('/detail/:idx', authUtils.LoggedIn, async (req,res,next) => {
 
     const idx = req.params.idx
+    if (!idx) {
+        res.status(400).json({
+            message : "idx가 비어있습니다."
+        })
+        return
+    }
     // const userEmail = req.userEmail 
     // let codeResult = await user.findOne({email:userEmail}).select({groupCode: 1})
-    let result = await post.findOne({_id : idx})
-    result.see += 1
-    result.score = (postResult.bookmark * 0.7 + postResult.see * 0.3)
-    let fin_result = await result.save().then(t => t.populate('comments').execPopulate())
+    try {
+        let result = await post.findOne({_id : idx})
+        result.see += 1
+        result.score = (postResult.bookmark * 0.7 + postResult.see * 0.3)
+        let fin_result = await result.save().then(t => t.populate('comments').execPopulate()).catch((err) => {
+            res.status(500).json({
+                message:"DB서버 에러",
+            })
+        })
 
-    res.status(200).json({
-        message: "해당 포스트 조회 성공",
-        data: {
-            pidArr : fin_result
-        }
-    })
+        res.status(200).json({
+            message: "해당 포스트 조회 성공",
+            data: {
+                pidArr : fin_result
+            }
+        })
+    } catch {
+        res.status(500).json({
+            message: "서버 에러",
+        })
+    }
+    
 })
 
 // 그룹의 전체 게시물 조회 # 완료
@@ -68,26 +92,33 @@ router.get('/', authUtils.LoggedIn, async (req,res,next) => {
         limit: req.query.limit || 10
     }
 
-    const userEmail = req.userEmail 
-    let codeResult = await user.findOne({email:userEmail}).select({groupCode: 1})
+    const userEmail = req.userEmail
+    try {
+        let codeResult = await user.findOne({email:userEmail}).select({groupCode: 1})
 
-    let result = await post.find({groupCode : codeResult.groupCode}).select({comments:0}).skip(pageOptions.page).limit(pageOptions.limit)
-
-    result.forEach((n) => {
-        let count = n.bookmark_list.findIndex(i => i == userEmail);
-        if(count !== -1) {
-            n.bookmarked = true
-        }else {
-            n.bookmarked = false  
-        }
-    })
-
-    res.status(200).json({
-        message: "전체 피드 조회 성공",
-        data: {
-            pidArr : result
-        }
-    })
+        let result = await post.find({groupCode : codeResult.groupCode}).select({comments:0}).skip(pageOptions.page).limit(pageOptions.limit)
+    
+        result.forEach((n) => {
+            let count = n.bookmark_list.findIndex(i => i == userEmail);
+            if(count !== -1) {
+                n.bookmarked = true
+            }else {
+                n.bookmarked = false  
+            }
+        })
+    
+        res.status(200).json({
+            message: "전체 피드 조회 성공",
+            data: {
+                pidArr : result
+            }
+        })
+    } catch {
+        res.status(500).json({
+            message: "서버 에러"
+        })
+    } 
+   
 })
 
 // 그룹의 해시태그로 조회 #완료
@@ -101,26 +132,34 @@ router.get('/hash', authUtils.LoggedIn, async (req,res,next) => {
     }
 
     const userEmail = req.userEmail 
-    let codeResult = await user.findOne({email:userEmail}).select({groupCode: 1})
 
- // 그룹 코드
-    let result = await post.find({groupCode : codeResult.groupCode, category : category}).skip(pageOptions.page).limit(pageOptions.limit)
+    try {
+        let codeResult = await user.findOne({email:userEmail}).select({groupCode: 1})
 
-    result.forEach((n) => {
-        let count = n.bookmark_list.findIndex(i => i == userEmail);
-        if(count !== -1) {
-            n.bookmarked = true
-        }else {
-            n.bookmarked = false  
-        }
-    })
+    // 그룹 코드
+        let result = await post.find({groupCode : codeResult.groupCode, category : category}).skip(pageOptions.page).limit(pageOptions.limit)
 
-    res.status(200).json({
-        message: "전체 피드 조회",
-        data: {
-            pidArr : result
-        }
-    })
+        result.forEach((n) => {
+            let count = n.bookmark_list.findIndex(i => i == userEmail);
+            if(count !== -1) {
+                n.bookmarked = true
+            }else {
+                n.bookmarked = false  
+            }
+        })
+
+        res.status(200).json({
+            message: "전체 피드 조회",
+            data: {
+                pidArr : result
+            }
+        })
+    } catch {
+        res.status(500).json({
+            message: "서버 에러"
+        })
+    }
+    
 
 })
 
@@ -136,53 +175,62 @@ router.post('/', authUtils.LoggedIn, upload.array('images'),async function(req, 
     } = req.body
 
     const userEmail = req.userEmail 
-    let codeResult = await user.findOne({email:userEmail}).select({groupCode: 1, name: 1, profileImage: 1})
 
-    const postImages = req.files
+    try {
+        let codeResult = await user.findOne({email:userEmail}).select({groupCode: 1, name: 1, profileImage: 1})
 
-    client.fetch(url, param,function(err, $, re){ 
-        var posts = new post()
-        
-        if (err){  
-            res.status(500).json({
-                message: "success",
-                data: {}
+        const postImages = req.files
+    
+        client.fetch(url, param,function(err, $, re){ 
+            var posts = new post()
+            
+            if (err){  
+                res.status(500).json({
+                    message: "크롤링 서버 에러",
+                })
+                return; 
+            }
+            
+            const image = $("meta[property='og:image']").attr('content')
+            const title = $("meta[property='og:title']").attr('content')
+            const description = $("meta[property='og:description']").attr('content')
+    
+            if (image) {
+                posts.image = image
+            } 
+    
+            if (title) {
+                posts.title = title
+            }
+    
+            if (description) {
+                posts.description = description
+            }
+    
+            posts.groupCode = codeResult.groupCode
+            posts.category = category
+            posts.writer = codeResult.name
+            posts.postContent = postContent
+            posts.profileImage = codeResult.profileImage
+            posts.url = url
+    
+            postImages.forEach((n) => {
+                posts.postImages.push(n.location)
             })
-            return; 
-        }
-        
-        const image = $("meta[property='og:image']").attr('content')
-        const title = $("meta[property='og:title']").attr('content')
-        const description = $("meta[property='og:description']").attr('content')
-
-        if (image) {
-            posts.image = image
-        } 
-
-        if (title) {
-            posts.title = title
-        }
-
-        if (description) {
-            posts.description = description
-        }
-
-        posts.groupCode = codeResult.groupCode
-        posts.category = category
-        posts.writer = codeResult.name
-        posts.postContent = postContent
-        posts.profileImage = codeResult.profileImage
-        posts.url = url
-
-        postImages.forEach((n) => {
-            posts.postImages.push(n.location)
+            
+            posts.save()
+            .then((result) => {
+                res.status(200).json({
+                    message: "게시물 업로드 완료",
+                })
+            }) 
+        });
+    } catch {
+        res.status(500).json({
+            message: "서버 에러"
         })
-        
-        posts.save()
-        .then((result) => {
-            res.json({message:result})
-        }) 
-    });
+    }
+  
 
 });
 
