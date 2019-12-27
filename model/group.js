218db15af41a2c4bcf74e2377497c290fcb33c92
donghunee <ehdgns1766@naver.com>
2019-12-28 01:12:56 +0900

    북마크 및 해당 게시물이 북마크 되었는지 확인

--- model/group.js
+++ model/group.js
@@ -5,7 +5,7 @@ var groupSchema = new Schema({
     name:{type: String, required : true}, //네이버.. 다음
     phone : {type: String, required :true}, // 010-5409-9859 
     department: String, // 직무 마케팅, 개발..
-    category:[{type:String, default:"flood"}],
+    category:[String],
     groupImage:String,
     groupCode:{
         type:String, 
