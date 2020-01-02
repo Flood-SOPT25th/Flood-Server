5b713da74153f67db3eaad63cf4d98bb70b150e2
Ju-Yeon Park <pjuyeon25@gmail.com>
2020-01-02 21:06:34 +0900

    random 번호추가(testing에 필요)

--- module/randomCode.js
+++ module/randomCode.js
@@ -8,4 +8,16 @@ module.exports ={
             groupCode += possible.charAt(Math.floor(Math.random() * possible.length));
         return groupCode+unixTime; // 5자리 그룹코드와 현재 시간 합
     }
+    ,randNumber:()=>{
+        var min = Math.ceil(min);
+        var max = Math.floor(max);
+
+        var phoneNumber = "010-";
+        for(i = 0; i < 4; i++)
+            phoneNumber +=  Math.floor(Math.random() * 10); 
+        phoneNumber += "-";
+        for(i = 0; i < 4; i++)
+            phoneNumber +=  Math.floor(Math.random() * 10); 
+        return phoneNumber;
+    }
 }
